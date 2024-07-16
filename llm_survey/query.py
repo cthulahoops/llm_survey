610627c575e768d40bf64d78e03b8f971280bc03
Adam Kelly <adam@cthulahoops.org>
2024-07-16 14:25:13 +0100

    Remove dead code from query.
    Query caching decorator no longer used.

--- llm_survey/query.py
+++ llm_survey/query.py
@@ -1,7 +1,5 @@
 import functools
-import json
 import os
-import sqlite3
 
 
 def log_request_details(f):
@@ -23,39 +21,9 @@ def get_openrouter_client():
     )
 
 
-def sqlite_cache(db_file):
-    def cache_decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            with sqlite3.connect(db_file) as conn:
-                c = conn.cursor()
-                c.execute(
-                    """CREATE TABLE IF NOT EXISTS cache (args TEXT PRIMARY KEY, result TEXT)"""
-                )
 def get_openai_client():
     import openai
 
-                jsoned_args = json.dumps(
-                    {
-                        "args": args,
-                        "kwargs": kwargs,
-                    }
-                )
-                c.execute("SELECT result FROM cache WHERE args=?", (jsoned_args,))
-                result = c.fetchone()
-                if result is not None:
-                    return json.loads(result[0])
-
-                result = func(*args, **kwargs)
-                c.execute(
-                    "INSERT INTO cache VALUES (?, ?)", (jsoned_args, result.to_json())
-                )
-                conn.commit()
-                return result
-
-        return wrapper
-
-    return cache_decorator
     return openai.Client(
         api_key=os.environ.get("OPENAI_API_KEY"),
     )
@@ -69,12 +37,6 @@ def get_completion(model, prompt):
             {"role": "user", "content": prompt},
         ],
     )
-
-
-@sqlite_cache("evaluation_cache.db")
-def get_model_response(model, prompt):
-    request_id, completion = get_completion(model, prompt)
-    return completion.choices[0].message.content
 
 
 @log_request_details
